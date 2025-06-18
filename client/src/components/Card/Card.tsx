e3a5bcedb8fe03a980ea8af6b706bbf5a7d9dbb1
Akacia <akacia@iMac-306.local>
2025-06-18 17:22:14 +0800

    set cards internal link

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,29 +1,75 @@
 import "./Card.scss";
+import { Link } from "@tanstack/react-router";
+import { createElement, ReactNode } from "react";
 
 export interface ICard {
-    title: string;
-    description: string;
-    type: "TOOL" | "TEMPLATE" | "MODULE" | "DOCUMENT" | "PLUGIN";
-    tag: number;
-    link: "EXTERNAL" | "DOWNLOAD" | "INTERNAL",
-    href: string;
-    picture: string;
+  title: string;
+  description: string;
+  type: "TOOL" | "TEMPLATE" | "MODULE" | "DOCUMENT" | "PLUGIN";
+  tag: number;
+  link: "EXTERNAL" | "DOWNLOAD" | "INTERNAL";
+  href: string;
+  picture: string;
 }
 
-export default ({ title, description, type, tag, link, href, picture }: ICard) => {
-
-    return (
-        <a className="card round" href={href} {...link == 'EXTERNAL' && {target:"_blank"}}>
-            <div className="card-picture">
-                <img src={picture} />
-            </div>
-
-            <div className="padding-l flex f-col gap-l">
-                <div className="flex f-col gap-s">
-                    <p className="subtitle-1">{title}</p>
-                </div>
-                <p className="card-desc body-2-regular">{description}</p>
-            </div>
-        </a>
-    );
+interface ICardLinkElement {
+  href: ICard["href"];
+  link: ICard["link"];
+  children: ReactNode;
 }
+
+interface ICardContent {
+  title: ICard["title"];
+  picture: ICard["picture"];
+  description: ICard["description"];
+}
+
+const AnchorElement = ({ href, link, children }: ICardLinkElement) => (
+  <a
+    className="card round"
+    href={href}
+    {...(link == "EXTERNAL" && { target: "_blank" })}
+  >
+    {children}
+  </a>
+);
+
+const RouterElement = ({ href, children }: ICardLinkElement) => (
+  <Link className="card round" to={href}>
+    {children}
+  </Link>
+);
+
+const CardContent = ({ picture, title, description }:ICardContent) => (
+  <>
+    <div className="card-picture">
+      <img src={picture} />
+    </div>
+
+    <div className="padding-l flex f-col gap-l">
+      <div className="flex f-col gap-s">
+        <p className="subtitle-1">{title}</p>
+      </div>
+      <p className="card-desc body-2-regular">{description}</p>
+    </div>
+  </>
+);
+
+export default ({
+  title,
+  description,
+  type,
+  tag,
+  link,
+  href,
+  picture,
+}: ICard) => {
+  const LinkElement = link == "INTERNAL" ? RouterElement : AnchorElement;
+  const LinkContent = createElement(CardContent, {
+    picture,
+    title,
+    description,
+  });
+
+  return createElement(LinkElement, { href, link, children: LinkContent });
+};
