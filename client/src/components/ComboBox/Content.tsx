be749371dd4c08f25ad68062f02432feb975acdb
Akacia <nassim.elkhantour@gmail.com>
2025-07-04 15:17:07 +0800

    fix dropdown

--- client/src/components/ComboBox/Content.tsx
+++ client/src/components/ComboBox/Content.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, useContext } from "react";
+import {
+  BaseSyntheticEvent,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
 import { ComboBoxContext, IComboBoxState } from "./Wrapper";
 import { createPortal } from "react-dom";
 
@@ -9,19 +15,36 @@ export interface IComboBoxContent {
 }
 
 export default ({ children, id, className }: IComboBoxContent) => {
-  const { comboBoxContent } = useContext(ComboBoxContext);
+  const { comboBoxContent, setComboBoxContent } = useContext(ComboBoxContext);
+  const panel = useRef<any>();
 
-  console.log(comboBoxContent);
+  useEffect(() => {
+    const handleMouseDown: EventListener = (e) => {
+      if (panel.current && !panel.current.contains(e.target)) {
+        // close combobox on backdrop click
+        setComboBoxContent((current) => ({
+          ...current,
+          id: undefined,
+          target: undefined,
+        }));
+      }
+    };
+
+    document.addEventListener("mouseup", handleMouseDown);
+
+    return () => document.removeEventListener("mouseup", handleMouseDown);
+  }, []);
 
   if (comboBoxContent.id !== id || comboBoxContent.target == undefined)
     return null;
 
   return createPortal(
     <div
-      className={`combobox-content panel ${className || ''}`}
+      className={`combobox-content panel ${className || ""}`}
       style={{
         transform: `translate3d(${comboBoxContent.target.getBoundingClientRect().left}px, ${comboBoxContent.target.getBoundingClientRect().bottom}px, 0px)`,
       }}
+      ref={panel}
     >
       {children}
     </div>,
